4de9b1de44b0a6a50fe686a1fae7e9e3873c840a
Paulo Sergio <psdsfj.snf20@uea.edu.br>
2021-05-19 20:23:02 -0400

    #Feature: Create empty screen

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+import pygame
+from pygame.locals import *
+from pygame.surface import Surface
+
+WINDOW_WIDTH = 720
+WINDOW_HEIGHT = 1000
+SCREEN_DIMENSION = (WINDOW_WIDTH, WINDOW_HEIGHT)
+
 if __name__ == '__main__':
-    print("Hello World")
+    window_surface: Surface = pygame.display.set_mode(SCREEN_DIMENSION)
+    pygame.display.set_caption("BREAKOUT")
+    pygame.display.flip()
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
