96b9ac7764ec1cf2f029b24e6d2236f83f512365
Glomyer <lcgpgj.snf20@uea.edu.br>
2021-05-22 14:59:59 -0400

    Base das classes Brick e Ball implementada

--- main.py
+++ main.py
@@ -9,6 +9,77 @@ WINDOW_WIDTH = 460
 WINDOW_HEIGHT = 720
 SCREEN_DIMENSION = (WINDOW_WIDTH, WINDOW_HEIGHT)
 
+PLAYER_SCORE = 0
+
+
+class Brick:
+    def __init__(self, x, y, color):
+        # x and y represents the coordinate where the brick will be placed
+        self.rect = pygame.Rect(x, y, 20, 5)
+        self.x = x
+        self.y = y
+        self.is_hidden = False
+        self.color = color
+
+    def increment_score(self):
+        global PLAYER_SCORE
+        # points added depends on the brick color
+        if self.color == "blue":
+            PLAYER_SCORE += 100
+        elif self.color == "green":
+            PLAYER_SCORE += 200
+        elif self.color == "yellow":
+            PLAYER_SCORE += 400
+        elif self.color == "orange":
+            PLAYER_SCORE += 800
+        elif self.color == "red":
+            PLAYER_SCORE += 1000
+
+    def hide_brick(self):
+        self.is_hidden = True
+
+
+class Ball:
+    def __init__(self, x, y):
+        self.rect = pygame.Rect(x, y, 20, 20)
+        self.dx = 1
+        self.dy = 1
+        self.speed = 5
+
+    def is_colliding_with_brick(self):
+        brick_list = []
+        for brick in brick_list:
+            if self.rect.colliderect(brick.rect) and self.dy < 0:
+                # makes the brick invisible after a collision
+                brick.hide_brick()
+                brick.increment_score()
+                # changes vertical direction and increases ball speed
+                self.dy *= -1
+                self.speed += 5
+
+                pygame.mixer.Sound('assets/bounce.wav').play()
+
+    def is_colliding_with_wall(self):
+        pass
+
+    def update(self):
+        self.rect.y += self.dy * self.speed
+        self.rect.x += self.dx * self.speed
+        self.is_colliding_with_brick()
+        self.is_colliding_with_wall()
+
+    def restart_position(self):
+        # restart ball position after it hits the bottom
+        pass
+
+    def render(self, screen: pygame.surface):
+        pygame.draw.ellipse(screen, (255, 255, 255), self.rect)
+
+
+pygame.mixer.init()
+pygame.mixer.music.load("assets/cheetahmen.wav")
+pygame.mixer.music.play(loops=-1)
+
 if __name__ == '__main__':
     window_surface: Surface = pygame.display.set_mode(SCREEN_DIMENSION)
     pygame.display.set_caption("BREAKOUT")
@@ -16,7 +87,7 @@ if __name__ == '__main__':
     running = True
 
     player_1 = Player(WINDOW_WIDTH / 2, WINDOW_HEIGHT - 60)
-    # rect(x,y, width, height)
+
     left_limit_rect = Rect(10, 10, 10, 700)
     right_limit_rect = Rect(440, 10, 10, 700)
     top_limit_rect = Rect(20, 10, 420, 20)
