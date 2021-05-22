5b3f7a8df32c681b02a6825f2d5b70aa21b89f58
Paulo Sergio <psdsfj.snf20@uea.edu.br>
2021-05-22 14:12:46 -0400

    #Refactor: extract border limits to objects

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ if __name__ == '__main__':
     running = True
 
     player_1 = Player(WINDOW_WIDTH / 2, WINDOW_HEIGHT - 100)
+    # rect(x,y, width, height)
+    left_limit_rect = Rect(10, 20, 10, 960)
+    right_limit_rect = Rect(690, 20, 10, 960)
+    top_limit_rect = Rect(20, 20, 680, 40)
 
     while running:
         for event in pygame.event.get():
@@ -33,11 +37,11 @@ if __name__ == '__main__':
                     player_1.is_going_right = False
 
         window_surface.fill(colors.BLACK)
-        pygame.draw.rect(window_surface, colors.WHITE, (20, 20, 680, 30))
-        pygame.draw.rect(window_surface, colors.WHITE, (20, 20, 10, 960))
-        pygame.draw.rect(window_surface, colors.WHITE, (700, 20, 10, 970))
+        pygame.draw.rect(window_surface, colors.WHITE, left_limit_rect)
+        pygame.draw.rect(window_surface, colors.WHITE, right_limit_rect)
+        pygame.draw.rect(window_surface, colors.WHITE, top_limit_rect)
 
-        player_1.move(30, 700)
+        player_1.move(left_limit_rect.right, right_limit_rect.left)
         player_1.render(window_surface)
 
         pygame.display.update()
