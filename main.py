9ca7d0c76a51dbb03ee25468658fadfe39feceba
Glomyer <lcgpgj.snf20@uea.edu.br>
2021-05-23 19:03:08 -0400

    #Feature: score counter on top of the screen

--- main.py
+++ main.py
@@ -8,7 +8,18 @@ from Brick import Brick
 from Constants import *
 from Player import Player
 
+
+def text_creator(text_value, x, y, font_size):
+    font = pygame.font.Font('assets/PressStart2P.ttf', font_size)
+    text = font.render(text_value, True, (255, 255, 255))
+    text_rect = text.get_rect()
+    text_rect.center = (x, y)
+    window_surface.blit(text, text_rect)
+    return text_rect
+
+
 if __name__ == '__main__':
+    pygame.init()
     window_surface: Surface = pygame.display.set_mode(SCREEN_DIMENSION)
     pygame.display.set_caption(WINDOW_CAPTION)
 
@@ -85,5 +96,6 @@ if __name__ == '__main__':
         ball_1.is_colliding_with_limits(right_limit_rect, left_limit_rect, top_limit_rect)
         ball_1.is_colliding_with_player(player_1)
         Brick.is_colliding_with_ball(ball_1, brick_list)
-
+        text_creator(f"SCORE: {Brick.player_score}", 180, 60, 24)
+        
         pygame.display.update()
